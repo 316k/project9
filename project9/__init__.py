0d5a1f4c1f600c4d41887f8ab78f9c96b7863b26
Nicolas Hurtubise <316k@legtux.org>
2015-11-27 16:29:49 -0500

    Stuff, again

---
+++ project9/__init__.py
@@ -0,0 +1,22 @@
+import sqlite3
+from flask import Flask, g
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'rkunhffrzrag'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        g.db = sqlite3.connect(':memory:')
+        with open('project9/scheme.sql', 'r') as sql:
+            g.db.executescript("".join(sql.readlines()))
+    return g.db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+from project9 import actions
