f653505663d96457dd80d19f623d9bcfc650b701
Nicolas Hurtubise <316k@legtux.org>
2015-12-02 14:49:59 -0500

    Corrects oracle stuff

--- project9/oracle-insert.sql
+++ project9/oracle-insert.sql
@@ -11,14 +11,14 @@ INSERT INTO categories VALUES(8, 'Caractéristiques des humains', 6);
 INSERT INTO categories VALUES(9, 'Questionnement', NULL);
 INSERT INTO categories VALUES(10, 'QCM', 9);
 
-INSERT INTO professeurs VALUES1, 'Claude', 'Frasson', 'ift2935');
-INSERT INTO professeurs VALUES2, 'Jackie', 'LaTruite', 'mot-de-passe');
-INSERT INTO professeurs VALUES3, 'Erick', 'Gamma', 'connect-four');
-INSERT INTO professeurs VALUES4, 'Esma', 'Aimeur', 'ift2935');
+INSERT INTO professeurs VALUES(1, 'Claude', 'Frasson', 'ift2935');
+INSERT INTO professeurs VALUES(2, 'Jackie', 'LaTruite', 'mot-de-passe');
+INSERT INTO professeurs VALUES(3, 'Erick', 'Gamma', 'connect-four');
+INSERT INTO professeurs VALUES(4, 'Esma', 'Aimeur', 'ift2935');
 
 INSERT INTO cours VALUES('IFT2935', 'Bases de données');
 
-INSERT INTO professeur_cours VALUES(1, 'IFT2935'),
+INSERT INTO professeur_cours VALUES(1, 'IFT2935');
 INSERT INTO professeur_cours VALUES(4, 'IFT2935');
 
 INSERT INTO partie_cours VALUES(1, 'Première partie : Introduction', 'IFT2935');
@@ -36,24 +36,24 @@ INSERT INTO questions VALUES(7, 'Quel est l''utilité de la thêta jointure ?', 
 INSERT INTO questions VALUES(8, 'Existe-t-il un opérateur nommé sesqui-jointure ?', 4);
 INSERT INTO questions VALUES(9, 'Un vice-président d''Hydro Québec a-t-il déjà suivi un cours de bases de données à l''UdeM avec Claude Frasson ?', 1);
 
-INSERT INTO reponses VALUES(NULL, 'Une sorte de champginon', 1, 0);
-INSERT INTO reponses VALUES(NULL, 'Une structure servant à organiser des données', 1, 1);
-INSERT INTO reponses VALUES(NULL, 'Pour organiser efficacement des données', 2, 1);
-INSERT INTO reponses VALUES(NULL, 'Pour faire de délicieux biscuits', 2, 0);
-INSERT INTO reponses VALUES(NULL, 'Un attribut pouvant ouvrir une porte', 3, 0);
-INSERT INTO reponses VALUES(NULL, 'Un attribut identifiant un tuple', 3, 1);
-INSERT INTO reponses VALUES(NULL, 'Un attribut comportant plusieurs sous-données', 4, 1);
-INSERT INTO reponses VALUES(NULL, 'Un attribut en matériaux renforcés', 4, 0);
-INSERT INTO reponses VALUES(NULL, 'Unique', 5, 1);
-INSERT INTO reponses VALUES(NULL, 'Additif', 5, 0);
-INSERT INTO reponses VALUES(NULL, ':D', 6, 0);
-INSERT INTO reponses VALUES(NULL, 'Sigma', 6, 1);
-INSERT INTO reponses VALUES(NULL, 'Elle sert à joindre deux tables sur une condition', 7, 1);
-INSERT INTO reponses VALUES(NULL, 'On peut en parsemer sur la dinde à Noël', 7, 0);
-INSERT INTO reponses VALUES(NULL, 'Vrai', 8, 0);
-INSERT INTO reponses VALUES(NULL, 'Faux', 8, 1);
-INSERT INTO reponses VALUES(NULL, 'Oui', 9, 1);
-INSERT INTO reponses VALUES(NULL, 'Non', 9, 0);
+INSERT INTO reponses VALUES(1, 'Une sorte de champginon', 1, 0);
+INSERT INTO reponses VALUES(2, 'Une structure servant à organiser des données', 1, 1);
+INSERT INTO reponses VALUES(3, 'Pour organiser efficacement des données', 2, 1);
+INSERT INTO reponses VALUES(4, 'Pour faire de délicieux biscuits', 2, 0);
+INSERT INTO reponses VALUES(5, 'Un attribut pouvant ouvrir une porte', 3, 0);
+INSERT INTO reponses VALUES(6, 'Un attribut identifiant un tuple', 3, 1);
+INSERT INTO reponses VALUES(7, 'Un attribut comportant plusieurs sous-données', 4, 1);
+INSERT INTO reponses VALUES(8, 'Un attribut en matériaux renforcés', 4, 0);
+INSERT INTO reponses VALUES(9, 'Unique', 5, 1);
+INSERT INTO reponses VALUES(10, 'Additif', 5, 0);
+INSERT INTO reponses VALUES(11, ':D', 6, 0);
+INSERT INTO reponses VALUES(12, 'Sigma', 6, 1);
+INSERT INTO reponses VALUES(13, 'Elle sert à joindre deux tables sur une condition', 7, 1);
+INSERT INTO reponses VALUES(14, 'On peut en parsemer sur la dinde à Noël', 7, 0);
+INSERT INTO reponses VALUES(15, 'Vrai', 8, 0);
+INSERT INTO reponses VALUES(16, 'Faux', 8, 1);
+INSERT INTO reponses VALUES(17, 'Oui', 9, 1);
+INSERT INTO reponses VALUES(18, 'Non', 9, 0);
 
 INSERT INTO question_categories
     SELECT id, 10 FROM questions;
