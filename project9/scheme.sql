cce6b9ced4f189005309354689beb9f347351553
Nicolas Hurtubise <316k@legtux.org>
2015-12-03 17:16:53 -0500

    Stuff

--- project9/scheme.sql
+++ project9/scheme.sql
@@ -12,7 +12,7 @@ CREATE TABLE cours(
     name TEXT NOT NULL
 );
 
--- Cours donnés par un professeur
+-- Cours donnÃ©s par un professeur
 CREATE TABLE professeur_cours(
     professeur_id INTEGER NOT NULL,
     cours_id TEXT NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE partie_cours(
     FOREIGN KEY(cours_id) REFERENCES cours(sigle)
 );
 
--- Questions à choix multiples
+-- Questions Ã  choix multiples
 CREATE TABLE questions(
     id INTEGER NOT NULL PRIMARY KEY,
     content TEXT,
@@ -39,7 +39,7 @@ CREATE TABLE questions(
     FOREIGN KEY(partie_cours_id) REFERENCES partie_cours(id)
 );
 
--- Réponses des questions
+-- RÃ©ponses des questions
 CREATE TABLE reponses(
     id INTEGER NOT NULL PRIMARY KEY,
     texte TEXT,
@@ -48,7 +48,7 @@ CREATE TABLE reponses(
     FOREIGN KEY(question_id) REFERENCES questions(id)
 );
 
--- Une catégorie peut avoir des sous-catégories de façon récursive
+-- Une catÃ©gorie peut avoir des sous-catÃ©gories de fa\E7on rÃ©cursive
 CREATE TABLE categories(
     id INTEGER NOT NULL PRIMARY KEY,
     name TEXT,
@@ -64,23 +64,20 @@ CREATE TABLE question_categories(
     PRIMARY KEY (question_id, category_id)
 );
 
--- Notes sur le schéma :
---  * les exercices reliés sont calculés à partir des catégories associées aux questions
---  * Il n'est pas nécessaire d'associer des sous-catégories à un cours, puisqu'on peut retrouver le cours en trouvant récursivement le parent (FIXME ?)
 -- Notes sur le schÃ©ma :
 --  * les exercices reliÃ©s sont calculÃ©s Ã  partir des catÃ©gories associÃ©es aux questions
 
 ---------------------
 -- Valeurs de test --
 INSERT INTO categories VALUES
-    (1, 'Normalisation des bases de données', NULL),
+    (1, 'Normalisation des bases de donnÃ©es', NULL),
     (2, 'Formes normales', 1),
     (3, '1FN', 2),
     (4, '2FN', 2),
     (5, '3FN', 2),
     (6, 'Interactions machine-humain', NULL),
-    (7, 'Caractéristiques du code', 6),
-    (8, 'Caractéristiques des humains', 6),
+    (7, 'CaractÃ©ristiques du code', 6),
+    (8, 'CaractÃ©ristiques des humains', 6),
     (9, 'Questionnement', NULL),
     (10, 'QCM', 9),
     (15, 'Syntaxe', NULL),
@@ -93,10 +90,10 @@ INSERT INTO professeurs VALUES
     (1, 'Claude', 'Frasson', 'ift2935'),
     (2, 'Jackie', 'LaTruite', 'mot-de-passe'),
     (3, 'Erick', 'Gamma', 'connect-four'),
-    (4, 'Esma', 'Aïmeur', 'ift2935');
+    (4, 'Esma', 'AÃ¯meur', 'ift2935');
 
 INSERT INTO cours VALUES
-    ('IFT2935', 'Bases de données');
+    ('IFT2935', 'Bases de donnÃ©es'),
     ('IFT1235', 'Programmation');
 
 INSERT INTO professeur_cours VALUES
@@ -106,38 +103,36 @@ INSERT INTO professeur_cours VALUES
     (6, 'IFT1235');
 
 INSERT INTO partie_cours VALUES
-    (1, 'Première partie : Introduction', 'IFT2935'),
-    (2, 'Deuxième partie : Modèle Entité Relation', 'IFT2935'),
-    (3, 'Troisième partie : Modèle Relationnel', 'IFT2935'),
-    (4, 'Quatrième partie : L''algèbre et le calcul Relationnel', 'IFT2935');
+    (1, 'PremiÃ¨re partie : Introduction', 'IFT2935'),
+    (2, 'DeuxiÃ¨me partie : ModÃ¨le EntitÃ© Relation', 'IFT2935'),
+    (3, 'TroisiÃ¨me partie : ModÃ¨le Relationnel', 'IFT2935'),
+    (4, 'QuatriÃ¨me partie : L''algÃ¨bre et le calcul Relationnel', 'IFT2935'),
     (10, 'Partie 1 : Introduction', 'IFT1235'),
     (11, 'Partie 2 : Structures de contrÃ´le', 'IFT1235'),
     (12, 'Partie 3 : Les procÃ©dures', 'IFT1235'),
     (13, 'Partie 4 : Programmation orientÃ©e objet', 'IFT1235');
 
 INSERT INTO questions(id, content, partie_cours_id) VALUES
-    (1, 'Qu''est-ce qu''une base de données ?', 1),
-    (2, 'Pourquoi a-t-on besoin de bases de données ?', 1),
-    (3, 'Qu''est-ce qu''un attribut clé ?', 2),
+    (1, 'Qu''est-ce qu''une base de donnÃ©es ?', 1),
+    (2, 'Pourquoi a-t-on besoin de bases de donnÃ©es ?', 1),
+    (3, 'Qu''est-ce qu''un attribut clÃ© ?', 2),
     (4, 'Qu''est-ce qu''un attribut composite ?', 2),
-    (5, 'Nommez un type de contrainte d''intégrité relationnelle', 3),
-    (6, 'Quel est le symbol pour l''opération de SÉLECTION ?', 3),
-    (7, 'Quel est l''utilité de la thêta jointure ?', 4),
-    (8, 'Existe-t-il un opérateur nommé sesqui-jointure ?', 4),
-    (9, 'Un vice-président d''Hydro Québec a-t-il déjà suivi un cours de bases de données à l''UdeM avec Claude Frasson ?', 1);
-
-    (10, 'Qu''est-ce qui est une relation dans un modèle E-R?', 2),
+    (5, 'Nommez un type de contrainte d''intÃ©gritÃ© relationnelle', 3),
+    (6, 'Quel est le symbol pour l''opÃ©ration de SÃ‰LECTION ?', 3),
+    (7, 'Quel est l''utilitÃ© de la thÃªta jointure ?', 4),
+    (8, 'Existe-t-il un opÃ©rateur nommÃ© sesqui-jointure ?', 4),
+    (9, 'Les bases de donnÃ©es permettent-elles de d''analyser un grand nombre d''informations ?', 1),
+    (10, 'Qu''est-ce qui est une relation dans un modÃ¨le E-R?', 2),
     (11, 'Qu''est-ce qu''une jointure?', 4),
-    (12, 'Qu''elle est la différence entre un modèle E-R et un modèle relationnel?', 3),
-    (13, 'Qu''elle est la différence entre une jointure et une thêta jointure?', 4),
-    (14, 'Pourquoi n''y a-t-il pas d''attributs multivalués dans un modèle relationnel?', 3),
-    (15, 'Qu''elle est l''utilité de l''implantation de la thêta jointure dans une base de donnée?', 4),
-    (16, 'Qu''est-ce qu''un attribut clé étrangère?', 2),
-    (17, 'Combien d''attributs clés peut contenir une table relationnelle?', 2),
-    (18, 'Comment se nomme l''opération servant à faire la jointure de deux tables selon un critère sous forme de booléen?', 4),
-    (19, 'Pourquoi devons-nous comprendre l''algèbre relationnel avant d''apprendre le langage SQL?', 4),
-    (20, 'Qu''elle est la différence entre une entité et une relation?', 2)
-    
+    (12, 'Qu''elle est la diffÃ©rence entre un modÃ¨le E-R et un modÃ¨le relationnel?', 3),
+    (13, 'Qu''elle est la diffÃ©rence entre une jointure et une thÃªta jointure?', 4),
+    (14, 'Pourquoi n''y a-t-il pas d''attributs multi-valuÃ©s dans un modÃ¨le relationnel?', 3),
+    (15, 'Quelle est l''utilitÃ© de l''implantation de la thÃªta-jointure dans une base de donnÃ©e?', 4),
+    (16, 'Qu''est-ce qu''un attribut clÃ© Ã©trangÃ¨re?', 2),
+    (17, 'Combien d''attributs clÃ©s peut contenir une table relationnelle?', 2),
+    (18, 'Comment se nomme l''opÃ©ration servant Ã  faire la jointure de deux tables selon un critÃ¨re sous forme de boolÃ©en?', 4),
+    (19, 'Pourquoi devons-nous comprendre l''algÃ¨bre relationnel avant d''apprendre le langage SQL?', 4),
+    (20, 'Quelle est la diffÃ©rence entre une entitÃ© et une relation?', 2),
     (30, 'Laquelle de ces dÃ©finitions reprÃ©sente une variable ?', 10),
     (31, 'Qu''es-ce qu''un interprÃ©teur ?', 10),
     (32, 'Que devrait-t-on mettre au dÃ©but d''un switch ?', 11),
@@ -148,20 +143,20 @@ INSERT INTO questions(id, content, partie_cours_id) VALUES
     (37, 'Quel est le premier paramÃ¨tre de toute mÃ©thode d''un objet en python ?', 13);
 
 INSERT INTO reponses VALUES
-    (NULL, 'Une sorte de champginon', 1, 0),
-    (NULL, 'Une structure servant à organiser des données', 1, 1),
-    (NULL, 'Pour organiser efficacement des données', 2, 1),
-    (NULL, 'Pour faire de délicieux biscuits', 2, 0),
-    (NULL, 'Un attribut pouvant ouvrir une porte', 3, 0),
+    (NULL, 'Une plateforme web', 1, 0),
+    (NULL, 'Une structure servant Ã  organiser des donnÃ©es', 1, 1),
+    (NULL, 'Pour organiser efficacement des donnÃ©es', 2, 1),
+    (NULL, 'Les bases de donnÃ©es ne servent Ã  rien', 2, 0),
+    (NULL, 'Un attribut quelconque', 3, 0),
     (NULL, 'Un attribut identifiant un tuple', 3, 1),
-    (NULL, 'Un attribut comportant plusieurs sous-données', 4, 1),
-    (NULL, 'Un attribut en matériaux renforcés', 4, 0),
+    (NULL, 'Un attribut comportant plusieurs sous-donnÃ©es', 4, 1),
+    (NULL, 'Un attribut multi-valulÃ©', 4, 0),
     (NULL, 'Unique', 5, 1),
     (NULL, 'Additif', 5, 0),
     (NULL, 'Pi', 6, 0),
     (NULL, 'Sigma', 6, 1),
-    (NULL, 'Elle sert à joindre deux tables sur une condition', 7, 1),
-    (NULL, 'On peut en parsemer sur la dinde à Noël', 7, 0),
+    (NULL, 'Elle sert Ã  joindre deux tables sur une condition', 7, 1),
+    (NULL, 'Elle sert Ã  joindre deux tables avec ayant un angle thÃªta minimal', 7, 0),
     (NULL, 'Vrai', 8, 0),
     (NULL, 'Faux', 8, 1),
     (NULL, 'Oui', 9, 1),
